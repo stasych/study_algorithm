57c0b948009ac1645c635f05cae6e7d08f595ea0
Stas <jobstas@gmail.com>
2014-03-18 18:26:21 +0400

    #2 доделал counting sort

--- hackerrank/algorithms/sorting/counting_sort_4_twist.py
+++ hackerrank/algorithms/sorting/counting_sort_4_twist.py
@@ -1,51 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 https://www.hackerrank.com/challenges/countingsort4
-sample successfull passed
-INPUT
-20
-0 ab
-6 cd
-0 ef
-6 gh
-4 ij
-0 ab
-6 cd
-0 ef
-6 gh
-0 ij
-4 that
-3 be
-0 to
-1 be
-5 question
-1 or
-2 not
-4 is
-2 to
-4 the
-
-RIGHT OUTPUT
-0 ab
-0 ef
-0 ab
-0 ef
-0 ij
-0 to
-1 be
-1 or
-2 not
-2 to
-3 be
-4 ij
-4 that
-4 is
-4 the
-5 question
-6 cd
-6 gh
-6 cd
-6 gh
+The Twist
 '''
 __author__ = 'Stas'
 
@@ -54,14 +10,16 @@ if __name__ == '__main__':
     k = 100
     num_lst = [[] for i in xrange(0,k)]
 
-
-
     for i in xrange(0,n):
         x,s = raw_input().split(' ')
         x = int(x)
-        num_lst[x].append(s)
+        if i < n/2:
+            num_lst[x].append('-')
+        else:
+            num_lst[x].append(s)
 
-
+    output = ''
     for x in xrange(0,k):
         for j in num_lst[x]:
-            print x,j
+            output += j+' '
+    print output[:-1]
