cfbfab3906b8910fb4e8632f5894c4a8702edcdd
Stas <jobstas@gmail.com>
2014-03-19 21:19:21 +0400

    #2 quick остался последний

---
+++ hackerrank/algorithms/sorting/quick_sort_partition.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+https://www.hackerrank.com/challenges/quicksort1
+'''
+__author__ = 'Stas'
+
+def partition(ar):
+    l = []
+    r = []
+
+    for x in ar:
+        if x<ar[0]:
+            l.append(x)
+        else:
+            r.append(x)
+    l.extend(r)
+    print ' '.join(map(str,l))
+
+if __name__ == '__main__':
+    n = input()
+    ar = map(int,raw_input().split())
+
+    partition(ar)
+
