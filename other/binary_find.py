57c12a4ba70ac4d470b56b91128297e9397f8f9a
Stas <jobstas@gmail.com>
2014-04-14 21:11:22 +0400

    #10 промежуточный коммит

--- other/binary_find.py
+++ other/binary_find.py
@@ -5,19 +5,30 @@
 __author__ = 'Stas'
 
 def binary_search(ar,z):
+    '''
+    возвращает индекс
+    спасибо центру за это
+    слова, после дороги начинают приобретать дополнительный смысл
+    '''
     rez = None
-    n = len(ar)-1
+
     if ar[0] == z:
         return 0
     if ar[-1] == z:
         return len(ar)-1
-    i = n/2
+
+    n = len(ar)
+    first = 0
+    i = n-1-first+1/2
+
+
     while not( i==0 or i == n):
-        print 'i=',i
         if ar[i] > z:
-            i = (0+i)/2
+            n = i-1
+            i = n-1-first+1/2
         elif ar[i]<z:
-            i = (i + n)/2
+            first = i+1
+            i = n-1-first+1/2
         elif ar[i] == z:
             rez = i
             break
@@ -25,6 +36,25 @@ def binary_search(ar,z):
 
 if __name__ == '__main__':
     lst = [10,11,12,13,14,15,16,17,18,19,20]
-    z = 17
-    rez = binary_search(lst,z)
-    print rez,lst[rez]
+
+    # case 1: first element
+    fisrt = 10
+    rez = binary_search(lst,fisrt)
+    print 'case 1: first element'
+    print 'rez i=',rez,'rez value:',lst[rez]
+
+    # case 2: middle element
+    middle = 15
+    rez = binary_search(lst,middle)
+    print 'case 3: middle element'
+    print 'rez i=',rez,'rez value:',lst[rez]
+
+    # case 3: last element
+    last = 20
+    rez = binary_search(lst,last)
+    print 'case 3: last element'
+    print 'rez i=',rez,'rez value:',lst[rez]
+
+    # case 4: wrong param
+
+    # case 5: dubles
