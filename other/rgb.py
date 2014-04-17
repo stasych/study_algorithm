0662a80f5a719073d622c55651ecef9df1b6084a
Stas <jobstas@gmail.com>
2014-04-17 20:23:06 +0400

    #4

--- other/rgb.py
+++ other/rgb.py
@@ -1,14 +1,12 @@
-# -*- coding:utf8 -*-
+# -*- coding: utf-8 -*-
 __author__ = 'stasych'
 
 '''
 Задачка из собеседования в Amazone
 '''
 
-if __name__ == '__main__':
-    data = 'BGBBRRGGBRGB'
-    lst = list(data)
-    print lst
+
+def div_list_to_3_part(lst):
     R,G,B = 'R','G','B'
     r,b = 0,0
     n = len(lst)
@@ -33,4 +31,34 @@ if __name__ == '__main__':
             lst[n-1-i] = B
             b -= 1
 
+if __name__ == '__main__':
+
+    print 'case 1: basic. even'
+    lst = list('BGBBRRGGBRGB')
+    print lst
+    div_list_to_3_part(lst)
+    print lst
+
+    print 'case 2: basic odd'
+    lst = list('BGBBR')
+    print lst
+    div_list_to_3_part(lst)
+    print lst
+
+    print 'case 3: cut'
+    lst = list('BR')
+    print lst
+    div_list_to_3_part(lst)
+    print lst
+
+    print 'case 4: one element'
+    lst = list('B')
+    print lst
+    div_list_to_3_part(lst)
+    print lst
+
+    print 'case 5: almosr sorted'
+    lst = list('GBRRRGGGGBBB')
+    print lst
+    div_list_to_3_part(lst)
     print lst
